a58e348c1e95c1f6c47de4972259620012c6e902
alyfer-mbr <alyferleandroaraujopedroso@gmail.com>
2024-09-05 14:59:55 -0300

    current: setting tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "vscode-dark": {
+          primary: "",
+        },
+        "vscode-light": {
+          primary: "",
+        },
+      },
+    },
   },
   plugins: [],
 };
